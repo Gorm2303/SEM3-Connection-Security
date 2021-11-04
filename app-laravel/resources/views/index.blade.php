e3fe00c898a93197aaaf77946a43c569e898fde3
Gorm2303 <gokri20@student.sdu.dk>
2021-11-04 09:32:58 +0100

    added settingscontroller & view

--- app-laravel/resources/views/index.blade.php
+++ app-laravel/resources/views/index.blade.php
@@ -6,12 +6,5 @@
     <a class="login-link" href="<?php echo route("login")?>">Login</a>
     <a class="register-link" href="<?php echo route("register")?>">Register</a>
 
-    <?php if (session()->has("success")): ?>
-        <div class="success-message"><?php echo session("success"); ?></div>
-    <?php elseif (session()->has("authenticated")): ?>
-        <div class="authenticated-message"><?php echo session("authenticated"); ?></div>
-    <?php elseif (session()->has("denied")): ?>
-        <div class="denied-message"><?php echo session("denied"); ?></div>
-    <?php endif; ?>
 @endsection
 
