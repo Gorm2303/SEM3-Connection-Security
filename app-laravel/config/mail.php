ad06e7ea8e78a66b93279b7c46c82c749483e2d2
Johan <johan.silenthill@gmail.com>
2021-11-05 15:32:35 +0100

    Mail system is set up, but connections is all wrong

--- app-laravel/config/mail.php
+++ app-laravel/config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'sem3.con.sec@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'SEM3 - Connection Security'),
     ],
 
     /*
